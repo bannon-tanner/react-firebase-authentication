373dc32bb3f41748c7688b59f6119f451055ec6f
bannon-tanner <bannon.tanner@perficient.com>
2022-04-14 10:13:01 -0500

    added login functionality and hide/show buttons in navbar whether logged in or not

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 This project is demonstrating authentication from [React - The Complete Guide (incl Hooks, React Router, Redux)](https://www.udemy.com/course/react-the-complete-guide-incl-redux/) using Firebase
 
+
+## Key Concepts
+
+Handle sign-up and login with the same fetch logic with firebase
+
+Use `!!variable` to return `true` or `false` on non-boolean variables
--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,10 +1,14 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useContext } from 'react'
 
+import AuthContext from '../../store/auth-context'
 import classes from './AuthForm.module.css'
 
 const AuthForm = () => {
   const emailInputRef = useRef()
   const passwordInputRef = useRef()
+
+  const authCtx = useContext(AuthContext)
+
   const [isLogin, setIsLogin] = useState(true)
   const [isLoading, setIsLoading] = useState(false)
 
@@ -20,37 +24,44 @@ const AuthForm = () => {
     // optional: add validation
 
     setIsLoading(true)
+    let url
 
     if (isLogin) {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.REACT_APP_FIREBASE_API_KEY}`
     } else {
-      fetch(
-        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_API_KEY}`,
-        {
-          method: 'POST',
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        }
-      ).then(res => {
+      url = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${process.env.REACT_APP_FIREBASE_API_KEY}`
+    }
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmail,
+        password: enteredPassword,
+        returnSecureToken: true,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((res) => {
         setIsLoading(false)
         if (res.ok) {
-
+          return res.json()
         } else {
-          return res.json().then(data => {
+          return res.json().then((data) => {
             let errorMessage = 'Authentication failed'
             if (data && data.error && data.error.message) {
               errorMessage = data.error.message
             }
-            alert(errorMessage)
+            throw new Error(errorMessage)
           })
         }
       })
-    }
+      .then((data) => {
+        authCtx.login(data.idToken)
+      })
+      .catch((err) => {
+        alert(err.message)
+      })
   }
 
   return (
@@ -71,7 +82,9 @@ const AuthForm = () => {
           />
         </div>
         <div className={classes.actions}>
-          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {!isLoading && (
+            <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          )}
           {isLoading && <p>Sending request...</p>}
           <button
             type="button"
